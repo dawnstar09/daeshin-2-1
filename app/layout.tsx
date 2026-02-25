ebd4bdbf896c85badeeedcd9e73b00c828dac48c
dawnstar09 <dawnstar09@naver.com>
2026-02-26 04:03:25 +0900

    message

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import ThemeProvider from '@/components/ThemeProvider'
 export const metadata: Metadata = {
   title: '대신고 2-5 - 클래스 관리',
   description: '대신고 2-5 반 학생들을 위한 스마트 클래스 관리 시스템',
+  icons: {
+    icon: '/icon.jpg',
+  },
 }
 
 export default function RootLayout({
