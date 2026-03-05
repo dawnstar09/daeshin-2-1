50e7275e5533bc4a9222dfdc6602a058c3067329
dawnstar09 <dawnstar09@naver.com>
2026-03-05 19:14:08 +0900

    Remove icon.jpg and icon reference

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,6 @@ import ThemeProvider from '@/components/ThemeProvider'
 export const metadata: Metadata = {
   title: '대신고 2-1 - 클래스 관리',
   description: '대신고 2-1 반 학생들을 위한 스마트 클래스 관리 시스템',
-  icons: {
-    icon: '/icon.jpg',
-  },
 }
 
 export default function RootLayout({
